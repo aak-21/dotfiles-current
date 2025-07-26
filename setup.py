d0edf7a3a900a76a1cfffbdffa3c1bf5d4761ac4
ak21 <abdoukab2008@gmail.com>
2025-07-26 23:23:25 +0100

    fixed script.py

--- setup.py
+++ setup.py
@@ -2,29 +2,16 @@ import os
 
 
 def repconf():
-    files = [
-        ".config/hypr/",
-        ".config/rofi/",
-        ".config/waybar/",
-        ".config/qt5ct/",
-        ".config/qt6ct/",
-        ".config/waybar/.config/nvim/",
-    ]
-    for file in files:
-        file = f"$HOME/{file}"
-        os.remove(file)
-    print("Deleting old config files completed !\n")
-    print("Now copying new config files\n")
-    os.system("cp .config .local $HOME/")
-    os.system("cp Wallpaper $HOME/Pictures/")
+    print("Copying new config files\n")
+    os.system("cp -r .config .local $HOME/")
+    os.system("cp -r Wallpapers $HOME/Pictures/")
     print("Finished !\nexiting .....")
     exit()
 
 
 print(
-    "This script is going to delete and replace your old config files for the following things :\n>hyprland\n>kitty\n>qt6ct\n>qt6ct\n>rofi\n>waybar\n>neovim"
+    "This script is going to replace your old config files for the following things :\n>hyprland\n>kitty\n>qt6ct\n>qt6ct\n>rofi\n>waybar\n>neovim"
 )
-yn = input("Are you sure you want to continue (y/n) : ")
 yes = ["y", "Y", "yes", ""]
 no = ["n", "N", "no"]
 
